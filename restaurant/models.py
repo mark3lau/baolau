8a40c9be2b3c38b8b4030a3e9b149e80153589d8
mark3lau <mark3lau@gmail.com>
2023-01-17 21:00:09 +0000

    create admin and reservations model

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Reservation(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    number_of_guests = models.IntegerField(max(0, 10), null=False, blank=False)
