bbfc2032a767ada89d9eb4172c3fa588f9a5f9bb
mark3lau <mark3lau@gmail.com>
2023-01-17 20:06:32 +0000

    connect make reservation template to the browser

--- baolau/urls.py
+++ baolau/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from restaurant.views import get_make_reservation
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', get_make_reservation, name='get_make_reservation')
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def get_make_reservation(request):
+    return render(request, 'reservation/make_reservation.html')
